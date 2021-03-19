d35368d4a1ab489560e914f4e6d5778ad496c6cd
Gabester0 <gceipper@gmail.com>
2021-03-19 18:24:01 -0400

    refactoring our decorators to use for server

--- src/features/metadata.ts
+++ src/features/metadata.ts
@@ -1,15 +1,45 @@
 import 'reflect-metadata';
+import { router } from '../routes/loginRoutes';
 
-const plane = {
-    color: "red"
-};
+@controller
+class Plane {
+    color: string = 'red';
 
-Reflect.defineMetadata('note', 'hi there I am red', plane, 'color');
-// Reflect.defineMetadata('note', 'hi there', plane);
-Reflect.defineMetadata('height', 10, plane);
+    @get('/login')
+    fly(): void {
+        console.log('vrrrr');
+    }
+}
 
-const note = Reflect.getMetadata('note', plane, 'color');
-// const note = Reflect.getMetadata('note', plane);
-const height = Reflect.getMetadata('height', plane);
-console.log(note)
-console.log(height)
+function get(path: string){
+    return function (target: Plane, key: string){
+        Reflect.defineMetadata('path', path, target, key)
+    }
+}
+
+function controller(target: typeof Plane){
+    for(let key in target.prototype){
+        const path = Reflect.getMetadata('path', target.prototype, key)
+        console.log(path)
+
+        router.get(path, target.prototype[key]);
+    }
+}
+
+
+
+// 1st Lesson, basics of metatdata:
+
+// const plane = {
+//     color: "red"
+// };
+
+// Reflect.defineMetadata('note', 'hi there I am red', plane, 'color');
+// // Reflect.defineMetadata('note', 'hi there', plane);
+// Reflect.defineMetadata('height', 10, plane);
+
+// const note = Reflect.getMetadata('note', plane, 'color');
+// // const note = Reflect.getMetadata('note', plane);
+// const height = Reflect.getMetadata('height', plane);
+// console.log(note)
+// console.log(height)
