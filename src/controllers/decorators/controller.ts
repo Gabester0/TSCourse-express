a988e35891fd7c088ae7aa735dee4c57d4403149
Gabester0 <gceipper@gmail.com>
2021-03-26 14:56:06 -0400

    set up use decorator for middleware

--- src/controllers/decorators/controller.ts
+++ src/controllers/decorators/controller.ts
@@ -9,11 +9,24 @@ export function controller(routePrefix: string){
         
         for(let key in target.prototype){
             const routeHandler = target.prototype[key];
-            const path = Reflect.getMetadata(MetadataKeys.Path, target.prototype, key);
-            const method: Methods = Reflect.getMetadata(MetadataKeys.Method, target.prototype, key);
+            const path = Reflect.getMetadata(
+                MetadataKeys.Path,
+                target.prototype,
+                key
+            );
+            const method: Methods = Reflect.getMetadata(
+                MetadataKeys.Method,
+                target.prototype,
+                key
+            );
+            const middlewares = Reflect.getMetadata(
+                MetadataKeys.Middleware,
+                target,
+                key
+            ) || [];
 
             if(path){
-                router[method](`${routePrefix}${path}`, routeHandler)
+                router[method](`${routePrefix}${path}`, ...middlewares, routeHandler)
             }
         }
     };
